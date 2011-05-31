cdbdc6a55349666c8e62f59a1cd1eeb3539ab5a1
areek <areek@fetchopia.com>
2011-05-30 21:43:33 -0400

    priceline area table mapper added

---
+++ sql/alchemy/priceline_area_table.py
@@ -0,0 +1,38 @@
+
+from sqlalchemy import *
+
+from alchemy_session import get_alchemy_info
+
+
+(engine, session, Base, metadata) = get_alchemy_info ()
+
+debug_level = 0
+
+#   Database Mapper Class for priceline_area table
+
+#   This table contains information about priceline areas
+
+class PricelineAreaTable(Base) :
+
+    __tablename__ = 'priceline_area_table'
+
+    uid = Column(Integer, Sequence('priceline_area_sequence'), primary_key=True)  #   primary key
+    name = Column(String()) #   name of the area
+
+
+    def __init__(self, uid, name) :
+
+        #   Assign params to member variables
+
+        self.uid = uid
+        self.name = name
+
+
+    def __str__(self) :
+
+        #   pretty printing
+
+        return "< priceline_area name = %s >" % (self.name)
+
+
+metadata.create_all(engine)
