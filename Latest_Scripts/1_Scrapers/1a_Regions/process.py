4599ca0935de47d9a9c2e3ffb56b5ae8202bd6f1
w24ma <david@fetchopia.com>
2012-02-28 18:46:24 -0500

    hotwire region parser

--- Latest_Scripts/1_Scrapers/1a_Regions/process.py
+++ Latest_Scripts/1_Scrapers/1a_Regions/process.py
@@ -1,130 +1,54 @@
-import MySQLdb
 import re
-import time
+from sqlalchemy import *
+from priceline_regions_table2 import PricelineRegionTable
+
+from alchemy_session import get_alchemy_info
+
+
+(engine, session, Base, metadata) = get_alchemy_info ()
 
+f = open("output/test_input_process.txt", "r")
 
-# Input is in the format "(region;regionid;cityid;latitude;longitude$)(point$)*"
-# Line are separated by $
-# Cities are separated by #
-
-
-# 1  Open the file and split into cities, initialize the mysql connection
-#
-input = open("output/process.txt", "r").read()
-input = input[1:]
-regions = re.split("#", input)
-output_detection = open("output/detection.txt","a")
-db_log = open("output/db_log.txt","a")
-db = MySQLdb.connect(user='root', passwd='areek', db='acuity_backup')
-c = db.cursor()
-
-# 2  Iterate through the cities and lines
-#    Skip the first city and last line because of formatting of input (they're empty)
-#
-
-#added
-c.execute('Select * from regionscitiesmap')
-existing_content = {}
-table_content = c.fetchall()
-
-if table_content:
-        for existing in table_content:
-            if int(existing[0]) not in existing_content.keys():
-                existing_content[int(existing[0])] = [int(existing[1])]
-            else:
-                existing_content[int(existing[0])].append(int(existing[1]))
-#edit ended
-                
-for region in regions:
-
-        # 3  Initialize necessary variables and perform required splits
-        #
-        is_in_db=0
-        region = region[:-1] # Remove an empty item at the end
+for line in f.readlines():
+    input = line
+    regions = re.split("#", input)
+    for region in regions:
+        region = region[:-1]
         lines = re.split("\$", region)
-        area = ''
         for index in range(len(lines)):
-                lines[index] = re.split(";", lines[index])
+            lines[index] = re.split(";", lines[index])
 
-        if len(lines[0]) == 6:
-            [name, id, star_aval, cityid, lat, lng] = lines[0]
+        if len(lines) == 1: ## case of empty line, ignore it
+            pass
         else:
-            [area, name, id, star_aval, cityid, lat, lng] = lines[0]
-   
-
-        if area:
-           
-            if area.find('located within the ')!=-1:
-                area = area[area.find('located within the ')+19:area.find('region')].strip()
-            
-            c.execute('''Select Priceline_area_id from priceline_area WHERE Name ='%s' ''' % area)
-
-            area_result = c.fetchall()
-
-            if not area_result:
-
-                c.execute("INSERT INTO `priceline_area` ( `Name` ) VALUES ( '"+area+"');")
-                db.commit()
-                c.execute('''Select Priceline_area_id from priceline_area WHERE Name = '%s' ''' % area)
-                area_id = str(c.fetchall()[0][0])
-                c.execute("INSERT INTO `priceline_area_city` ( `area_id`,`city_id` ) VALUES ( '"+area_id+"','"+cityid+"');")
-                db.commit()
-
-            else:
-                area_result = str(area_result[0][0])
-                c.execute("Select * from priceline_area_city where area_id = %s and city_id = %s" % (area_result,cityid))
-                if not c.fetchall():
-                    c.execute("INSERT INTO `priceline_area_city` ( `area_id`,`city_id` ) VALUES ( '"+area_result+"','"+cityid+"');")
-                    db.commit()
-
-
-        
-        print [area, name, id, star_aval, cityid, lat, lng]
-
-
-        
-        lines = lines[1:] # Put the list of points into line (element 0 is info outlined above)
-        c.execute('''Select * from pricelineregions WHERE pricelineid=%s ''' % id)
-        result = c.fetchall()
-        if not result:
-            output_detection.write('Region name: '+str(name)+ ' Not Found!')
-            is_in_db=0
-        else:
-            for one in result:
-
-                #   region does not have star_availibility information
-                #
-
-                if not one[7] and star_aval:
-                    c.execute("UPDATE `acuity_backup`.`pricelineregions` SET `Star_availibility` = '"+star_aval+"' WHERE `pricelineid` = '"+id+"' ;")
-            is_in_db = 1
-
-        if not is_in_db:
-                # 4b Inserts the new region and gets its regionid for use with the points
-                #
-                c.execute('''INSERT INTO pricelineregions (regionname, pricelineid, cities_cityid, latitude, longitude, active, Star_availibility) VALUES
-                ("%s",'%s','%s','%s','%s',%s)''' % (name, id, cityid, lat, lng, 1,star_aval))
-                c.execute("SELECT pricelineregionid FROM pricelineregions ORDER BY pricelineregionid DESC LIMIT 1")
-                regionid = c.fetchone()[0]
-                
-                # 6c Inserts the new points into the table if there aren't any others for that region
-                #
-                for i in range(len(lines)):
-                        line = lines[i]
-                        plat = line[2]
-                        plng = line[3]
-                        c.execute('''INSERT INTO pricelinepoints (pricelineregions_pricelineregionid, orderid, latitude, longitude) VALUES
-                        ("%s","%s","%s","%s")'''% (regionid, i, plat, plng))
-        #edited
-                c.execute('''INSERT INTO `regionscitiesmap` (`cities_cityid`, `priceline_regionid`)VALUES('%s', '%s');''' %(cityid,id))
-                db.commit()
-        
-        else:
-                if int(cityid) in existing_content.keys():
-                        if int(id) not in existing_content[int(cityid)]:
-                                c.execute('''INSERT INTO `regionscitiesmap` (`cities_cityid`, `priceline_regionid`)VALUES('%s', '%s');''' %(cityid,id))
-                                db.commit()
+            [name, city_state, lat, lng] = lines[0]
+            points = []
+            lines = lines[1:] ## only points in the line in line now
+            for i in range(len(lines)):
+                if len(lines[i]) == 1:
+                    pass
                 else:
-                        c.execute('''INSERT INTO `regionscitiesmap` (`cities_cityid`, `priceline_regionid`)VALUES('%s', '%s');''' %(cityid,id))
-                        db.commit()
-		
+                    line = lines[i]
+                    plat = line[2]
+                    plat = float(plat)
+                    plng = line[3]
+                    plng = float(plng)
+                    pair = (plat, plng)
+                    points.append(pair)
+            points = str(points)
+            city = re.search(".*\,", city_state).group()
+            city = re.sub("\,", "", city)
+            state = re.search("\,.*", city_state).group()
+            state = re.sub("\,", "", state)
+            print name, city, state
+            print points
+            entry = PricelineRegionTable(default, name, city, state, lat, lng, points)
+            session.add(entry)
+            session.commit()
+                    
+                    
+            
+        
+
+            
+
