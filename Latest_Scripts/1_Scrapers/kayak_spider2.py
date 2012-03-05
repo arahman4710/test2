e3c8475ca58738ece5c8bc43261dc69ff2b8bffe
w24ma <david@fetchopia.com>
2012-03-05 13:55:46 -0500

    latest kayak spider update

--- Latest_Scripts/1_Scrapers/kayak_spider2.py
+++ Latest_Scripts/1_Scrapers/kayak_spider2.py
@@ -1,13 +1,13 @@
 from decimal import *
 import Levenshtein
 from scrapy.selector import HtmlXPathSelector,XmlXPathSelector
+from scrapy.http import Request
 from scrapy.http import FormRequest
 from scrapy.conf import settings
 from scrapy.crawler import CrawlerProcess
 from scrapy.spider import BaseSpider
 from scrapy import signals,log
 from scrapy.xlib.pydispatch import dispatcher
-from scrapy.http import Request
 from scrapy.contrib.spiders import XMLFeedSpider
 import re
 from datetime import date
@@ -23,7 +23,7 @@ class KayakSpider(BaseSpider):
     name = "KayakSpider"
     start_urls = ["http://www.kayak.com/hotels"];
     allowed_domains = ["kayak.com"];
-    selec_city=['Houston'];
+    selec_city=['Miami, FL'];
     results=[];
     query_price=[];
     price_results={};
@@ -36,58 +36,55 @@ class KayakSpider(BaseSpider):
             city_request = city
             search = {'othercity': city_request, 'checkin_date': start_date, 'checkout_date': end_date}
 
-            request = FormRequest.from_response(response, formdata = search, callback = self.callfilter)
+            request = FormRequest.from_response(response, formnumber = 1, formdata = search, callback = self.do_city)
             request.meta['city'] = city
             yield request
 
+    def do_city(self, response):
+        ## iterates through the pages of hotels within a city
+        url = response.url
+        hxs = HtmlXPathSelector(response)
+        total_hotels_in_city = hxs.select("//span[@id='showingnumber']/text()")
+        pages = int(total_hotels_in_city.extract()[0]) / 15 ##find out how many pages of hotels to iterate through within city
+        x = 0
+        while x <= pages:
+            y = str(x)
+            pattern = "&pn=0"
+            new_pattern = re.sub("0", y, pattern)
+            nextpage_request = Request(url + new_pattern, callback = self.callfilter)
+            nextpage_request.meta['city'] = response.request.meta['city']
+            x += 1
+            yield nextpage_request
+            
     def callfilter(self, response):
 
-        query = re.search('(?<=\/)[^/]+($|\?)', response.url)
-        query = query.group()
-#        print response.url
         hxs = HtmlXPathSelector(response)
-        temp_url=hxs.select(".//*[contains(@id,'priceAnchor')]/a/@target").extract()
-        url = ''
-        for one in temp_url:
-            print str(one)
-            url += str(one)
-        request = Request("http://www.kayak.com/s/jsresults?ss=1&searchid=" + url[url.find('bookit_')+7:url.find('_',url.find('bookit_')+12,len(url))] + "&pn=0", callback = self.filtered)
-        request.meta['city'] = response.request.meta['city']
-        request.meta['page'] = 0
-        return request
+        hotel_list = hxs.select("//div[contains(@id, 'tbd')]/@data-resultid")
+        for hotel in hotel_list:
+            hid = hotel.extract() ##unique hotel id from kayak
+            request = Request("http://www.kayak.com/h/hotel/details.vtl?hid=" + hid, callback = self.filtered) 
+            request.meta['city'] = response.request.meta['city']
+            request.meta['hid'] = hid
+            yield request
 
     def filtered(self, response):
 
         doc = printable(response.body)
         hxs = HtmlXPathSelector(response)
-        hotels = hxs.select("//div[contains(@id, 'tbd') and not(contains(@class, 'opaque'))]/div[@class='inner']")
-        found = 0
+        
+        name = hxs.select(".//div[@class='overviewDataName']/text()")
+        name = name.extract()[0]
+        address = hxs.select(".//div[@class='address']/text()")
+        address = address.extract()[0]
+        price = hxs.select(".//span[@class='hoteldetailPrice']/text()")
+        price = price.extract()[0]
 
-        for div in hotels:
-            id = div.select(".//div[contains(@id, 'resultid')]/text()")
-            name = div.select(".//div[contains(@id, 'hname')]/text()")
-            address = div.select(".//a[@class='subtlelink'][1]/text()")
-            star = div.select(".//div[contains(@class,'starsprite')]/@class")
-            price = div.select(".//span[contains(@id,'priceAnchor')]/a/text()")
-            if (id and name and address and price):
-                found = 1
-                id = id[0].extract()
-                name = name[0].extract()
-                address = address[0].extract()
-                price = price[0].extract()
-                print id, name, address, price
+        star = hxs.select(".//div[contains(@class,'starsprite')]/@class")
+        if star:
+            star = star.extract()[0]
+            star = star[-1:]
 
-        if found:
-            url = response.url
-            page = response.request.meta['page']
-            url = re.sub("pn.*?(&|$)", "pn=" + str(page + 1) + "&", response.url)
-
-            request = Request(url, callback = self.filtered)
-            request.meta['city'] = response.request.meta['city']
-            request.meta['page'] = page + 1
-            return request
-        else:
-            pass
+        print name, address, star, price
 
 def main():
     log.start()
