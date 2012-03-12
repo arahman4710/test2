a24c3b91be4aa80d7b379b5d789bc6bd8517dbbe
w24ma <david@fetchopia.com>
2012-03-11 21:53:47 -0400

    latest hotwire

--- Latest_Scripts/2_Matching/2c_hotwire_matching/hotwire_matcher.py
+++ Latest_Scripts/2_Matching/2c_hotwire_matching/hotwire_matcher.py
@@ -22,18 +22,51 @@ def get_hotwire_amenity_info(amenity_list_id):
 	
 	items = session.query(AmenityListItem).filter(AmenityListItem.amenity_list_id == amenity_list_id)
 	items_list_id = []
-	for item in items:
+	for item in items.all():
 		items_list_id.append(item.amenity_id)
 	
 	
-	items_list_code = []
+	items_list_code = [] ##the amenity of this hotel in a list of codes ie. Airport Shuttle for AS and Near Beach for BA
 	for instance in items_list_id:
 		amenity_item = session.query(Amenity).filter(Amenity.uid == instance).first()
 		items_list_code.append(str(amenity_item.code))
 	
 	return items_list_code
 		
+def generate_amenity_list(amenity):
+	amenities_element_dict = {"Airport Shuttle":"AS", "Shuttle":"AS", "Business Center":"BC", "Business":"BC", "Boutique":"BH", "Complimentary Breakfast":"CB", "Fitness":"FC", "Fitness Center":"FC", 
+	"Hi-Speed Internet":"HS", "High-Speed Internet Access":"HS", "High Speed Internet":"HS", "High-Speed internet":"HS", "High-Speed Internet":"HS", "Kitchenette":"KI", "Laundry":"LF", "Pool":"PO", 		"Restaurant":"RE", "Spa":"SF", "Smoke Free":"SR", "Suite":"SU", "Golf":"GN", "Slopeside":"SL", "Oceanfront":"OF", "AC":"AC", "Parking":"FP", "Suites":"SU", "Golf Nearby":"GN",
+	"Tennis":"TN", "Near Beach":"BA", "Beachfront":"BF", "Casino":"CA", "Childrens Activities":"CH", "Resort":"RS", "A/C":"AC", "Condo":"C", "Kitchen":"KI", "24hr Front Desk":"HF", "Studio":"ST",
+	"Front Desk":"FD", "1BR":"1B", "Daily Housekeeping":"DH", "Free Parking":"FP", "Laundry (in room)":"WD", "Smoke-free":"SR", "Smoke Free Rooms":"SR", "Smoke-Free":"SR", "Smoke free":"SR",
+	"Pools":"PO", "Restaurants":"RE", "Hi-Speed internet":"HS", "24 Hour Front Desk":"HF", "Fully Equipped Kitchen":"FK", "Hi Speed Internet":"HS", "HI-Speed Internet":"HS",
+	##spelling mistake cases
+	"Suttlle":"AS", "Shuttlle":"AS",
+	"Bussines":"BC",
+	"Restuarant":"RE", "Restauraunt":"RE", "Restauant":"RE", "Restraurant":"RE", "Restaraunt":"RE", "Restaruant":"RE",
+	"Fintess":"FC",
+	"Complimetnary Breakfast":"CB", "Complementary Breakfast":"CB", "Comlimentary Breakfast":"CB", "Complimentry Breakfast":"CB", "Complimentary breakfast":"CB", "Comlimentary Breakfast":"CB",
+	"Complimentry Breakfast":"CB",
+	"Hi-Speed Interent":"HS", "Hi-Speed Intenet":"HS",
+	"Pooll":"PO",
+	"Kitchnette":"KI", "Kithenette":"KI", "Kithchenette":"KI",
+	"Laundy":"LF", "Laudry":"LF",
+	"Front Deks":"FD",
+	"Children Activities":"CH",
+	"Spa)":"SF",
+	"&lt;none supplied by user&gt;":"None", "&lt;none shown by hotwire&gt;":"None", "&lt;none shown by Hotwire&gt;":"None", "&lt;none supplied&gt;":"None"	
+	}
 	
+	amenity_items = re.split("\,", amenity)
+	items_list_code = [] 
+	for item in amenity_items:
+		item = re.sub("^\s+", "", item) ##remove whitespace infront of amenity element
+		item = re.sub("\s+$", "", item)
+		if item in amenities_element_dict.keys():
+			items_list_code.append(amenities_element_dict[item])
+	
+	return items_list_code
+
+		
 
 
 for instance in all_hotwire_hotels.all():
@@ -44,16 +77,14 @@ for instance in all_hotwire_hotels.all():
 	region_in_city_detect = re.search(".*(?=\()", bbhw_hotel_city)
 	if region_in_city_detect:
 		bbhw_hotel_city = region_in_city_detect.group()
+	
+	bb_amenity_info = generate_amenity_list(instance.amenities) ## make amenity list func turns raw amenity info into a list to compare
 		
 	possible_match = session.query(HotWireHotelInfo).filter(HotWireHotelInfo.city == bbhw_hotel_city)
 	
 	for match in possible_match:
 	
-		amenity_info = get_amenity_info() ##function that gets amenity info from hotwirehotelinfo table's amenity_list column
-		
-		session.query(AmenityListItem).filter(AmenityListItem.amenity_list_id == '18229').all()
-		
-		bb_amenity_info = make_amenity_list(instance.amenity) ## make amenity list func turns raw amenity info into a list to compare
+		amenity_info = get_hotwire_amenity_info(match.amenity_list) ##function that gets amenity info from hotwirehotelinfo 
 		
 		amenity_match_flag = False
 		star_match_flag = False
